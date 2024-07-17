c6dcd54c923087d011352d75e446252001caa93c
pc <pc@DESKTOP-78M62IA>
2024-07-17 16:45:39 +0300

    Add project files.

---
+++ PatikaWeek1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Fatih Sezgin/n Yazılım konusunda kendimi geliştirmek istediğim için işin ehli olan Patikadayım");
+        }
+        
+    }
+}
